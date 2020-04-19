e7126041f92fca7ea8442dfde6f9b2ab9b6bea57
Sulfudie <62355327+Sulfudie@users.noreply.github.com>
2020-04-19 22:06:45 +0200

    Update Credits.java

--- Credits.java
+++ Credits.java
@@ -1,23 +1,36 @@
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.*;
+import javax.swing.*; // Pour les composants graphiques que l'on ajoutera dans la méthode creerWidget
+import java.awt.*;    // Pour la JFrame++++++++++++++++++
+ 
+public class Credits extends JFrame {
 
-public class Credits {
-	public static void main (String[] args){
+  public Credits() {
+ 
+    creerWidget();
+ 
+    setSize(500,500);                                // Fixe la taille par défaut
+    setVisible(true);                                // Affiche la fenetre
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Gestion de la fermeture
+  }
+ 
+  public void creerWidget() {
+	
 
-		public void Credits(Graphics g) {
-			String remerciement = " Remerciement à notre tuteur JCC puis à machin machin";
-			String musique = " Musique utilisées : Nyan Cat, Harry Potter etc ";
-			String image = " images de Nyan Cat etc ";
-			String auteur = " Ce projet a été produit par Julien, Théo, Marion, Lisa, Nathan et Arthur";
-			String copyright = " Copyright machin truc ";
+		// un texte contenant un retour à la ligne
+  	String text0 = "Credits";
+	String text1 = "Le jeu a ete concu par Julien, Lisa, Marion, Theo, Nathan, Arthur";
+	String text2 = "La musique utilisee est celle de Harry potter, Nyan Cat etc";
+	String text3 = "Pour la realisation de ce jeu on tient a remercier notre tuteur monsieur Couchot etc. ";
+	 
+	JTextArea taText0 = new JTextArea(text0); 
+	JTextArea taText1 = new JTextArea(text1);
+	JTextArea taText2 = new JTextArea(text2);
+	JTextArea taText3 = new JTextArea(text3);
 
-			  g.setColor(Color.WHITE);
-		      g.drawString(remerciement,480, 20);
-		      g.drawString(musique, 480, 40);
-		      g.drawString(image,480, 20);
-		      g.drawString(auteur, 480, 40);
-		      g.drawString(copyright,480, 20);
-		}
+	JPanel pano = new JPanel(new GridLayout(4,1));
+	pano.add(taText0, BorderLayout.SOUTH);
+	pano.add(taText1);   // Création d'un JPanel qui va contenir
+	pano.add(taText2);
+	pano.add(taText3);
+	setContentPane(pano);         // Ajoute pano à la fenêtre principale
 	}
 }
