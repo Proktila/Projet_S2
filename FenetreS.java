3a0c25ff52babb8e54ce397ce89aa988860e5457
theoGANARD <theo.ganard@gmail.com>
2020-04-22 15:15:13 +0200

    ajout des attributs et methodes d'une première version du menu PARAMETRES (a changer de place ?)

--- FenetreS.java
+++ FenetreS.java
@@ -7,6 +7,15 @@ public class FenetreS extends JFrame {
     // Nécessaire à la crétion d'un JFrame :
     private static final long serialVersionUID = 1L;
 
+    // Elements du menu PARAMETRE
+    protected JSlider volumeMusique;
+    protected JSlider volumeBruits;
+    protected JRadioButton rbFrancais;
+    protected JRadioButton rbAnglais;
+    static final int VOL_MIN = 0;
+    static final int VOL_MAX = 100;
+    static final int VOL_INIT = 75;
+
     // Constructeur de la fenêtre :
     public FenetreS() {
 
@@ -31,5 +40,65 @@ public class FenetreS extends JFrame {
         // Permet de rendre la fenêtre visible (toujours placer à la fin)
         this.setVisible(true);
 
+    }
+
+
+    public void initParametres() {
+        volumeMusique = new JSlider(JSlider.HORIZONTAL, VOL_MIN, VOL_MAX, VOL_INIT);
+        volumeBruits = new JSlider(JSlider.HORIZONTAL, VOL_MIN, VOL_MAX, VOL_INIT);
+
+        volumeMusique.setMajorTickSpacing(10);
+        volumeMusique.setMinorTickSpacing(5);
+        volumeMusique.setPaintTicks(true);
+        volumeMusique.setPaintLabels(true);
+
+        volumeBruits.setMajorTickSpacing(10);
+        volumeBruits.setMinorTickSpacing(5);
+        volumeBruits.setPaintTicks(true);
+        volumeBruits.setPaintLabels(true);
+
+        rbFrancais = new JRadioButton("Français", true);
+        rbAnglais = new JRadioButton("English", false);
+        ButtonGroup rbLangue = new ButtonGroup();
+        rbAnglais.add(rbFrancais);
+        rbAnglais.add(rbAnglais);
+    }
+
+    public void creerParametresVue() {
+        JLabel lSon = new JLabel("Son");
+        JLabel lMusique = new JLabel("Musique");
+        JLabel lBruitages = new JLabel("Bruitages");
+        JLabel lLangue = new JLabel("Langue");
+
+        JPanel panRbLangue = new JPanel();
+        panRbAnglais.setLayout(new BoxLayout(panRbAnglais, BoxLayout.X_AXIS));
+        panRbAnglais.add(rbFrancais);
+        panRbAnglais.add(rbAnglais);
+        JPanel panLangues = new JPanel();
+        panAnglais.add(lLangue);
+        panAnglais.add(panRbLangue);
+
+        panGrid.add(lSon);
+        JPanel panGrid = new JPanel(new GridLayout(3,2));
+        panGrid.add(lMusique);
+        panGrid.add(slSon);
+        panGrid.add(lBruitages);
+        panGrid.add(slBruits);
+        panGrid.add(lLangue);
+        panGrid.add(panLangues);
+
+        JPanel panBorder = new JPanel();
+        panBorder.setBorder(BorderFactory.createLineBorder(Color.black));
+        panBorder.add(panGrid);
+
+        JPanel panDecalage = new JPanel();
+        panDecalage.setLayout(new BoxLayout(panDecalage, BoxLayout.Y_AXIS));
+        panDecalage.add(lSon);
+        panDecalage.add(panBorder);
+
+        JPanel panParametre = new JPanel();
+        panParametre.add(panDecalage);
+
+        setContentPane(panParametre);
     }
 }
