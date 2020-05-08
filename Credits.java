6c328e544e085e17df6f46d2203f8f96a710b97e
Sulfudie <62355327+Sulfudie@users.noreply.github.com>
2020-05-08 14:53:45 +0200

    Update Credits.java

--- Credits.java
+++ Credits.java
@@ -2,44 +2,53 @@ import javax.swing.*; // Pour les composants graphiques que l'on ajoutera dans l
 import java.awt.*;    // Pour la JFrame++++++++++++++++++
 import java.awt.Color;
 import javax.swing.JFrame;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
  
 public class Credits extends JFrame {
 
-  public Credits() {
- 
-    creerWidget();
- 	
- 	this.setTitle("Page credit du jeu");
-    setSize(500,600);                                // Fixe la taille par défaut
-    setVisible(true);                                // Affiche la fenetre
+    private Color green = new Color(50,99,23);
+    private JButton retour;
 
-    //JPanel pano = new JPanel();                       // Pour mettre un arrière plan de couleur
-    //pano.setBackground(Color.BLACK);
-    //this.setContentPane(pano);                  
+  public Credits() {
+
+ 	setTitle("Page credit du jeu"); 
+
+ 	initAttributs();
+
+    setSize(1280,720);                                // Fixe la taille par défaut        
+    setVisible(true);    
 
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Gestion de la fermeture
   }
- 
-  public void creerWidget() {
-	
 
-		// un texte contenant un retour à la ligne
-  	String text0 = "Credits";
-	String text1 = "Le jeu a ete concu par Julien, Lisa, Marion, Theo, Nathan, Arthur";
-	String text2 = "La musique utilisee est celle de Harry potter, Nyan Cat etc";
-	String text3 = "Pour la realisation de ce jeu on tient a remercier notre tuteur monsieur Couchot etc. ";
+
+  public void initAttributs() {
+
 	 
-	JTextArea taText0 = new JTextArea(text0); 
-	JTextArea taText1 = new JTextArea(text1);
-	JTextArea taText2 = new JTextArea(text2);
-	JTextArea taText3 = new JTextArea(text3);
+	JLabel taText0 = new JLabel("CREDITS"); 
+	taText0.setFont(new java.awt.Font("Monospaced", Font.BOLD,50));
+	JLabel taText1 = new JLabel("Le jeu à été concu par Julien, Lisa, Marion, Theo, Nathan, Arthur");
+	taText1.setFont(new java.awt.Font("Monospaced", Font.BOLD,20));
+	JLabel taText2 = new JLabel("La musique utilisee est celle de Harry potter, Nyan Cat etc");
+	taText2.setFont(new java.awt.Font("Monospaced", Font.BOLD,20));
+	JLabel taText3 = new JLabel("Pour la realisation de ce jeu on tient a remercier notre tuteur monsieur Couchot etc. ");
+	taText3.setFont(new java.awt.Font("Monospaced", Font.BOLD,20));
+
+	JButton but1 = new JButton("Retour Menu");
+
 
 	JPanel pano = new JPanel(new FlowLayout(FlowLayout.CENTER, 4000 ,100));
+
+
+	pano.setBackground(green);
 	pano.add(taText0);
 	pano.add(taText1);   // Création d'un JPanel qui va contenir
 	pano.add(taText2);
 	pano.add(taText3);
+	pano.add(but1);
+
+
 	setContentPane(pano);         // Ajoute pano à la fenêtre principale
 	}
 }
