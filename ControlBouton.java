288a4ec2b04744a244210954c46ada8d3891eb3e
lisa <doyenlisa@gmail.com>
2020-06-06 02:34:58 +0200

    maj + initalisation controlSnake

--- ControlBouton.java
+++ ControlBouton.java
@@ -8,23 +8,24 @@ import java.sql.SQLOutput;
 
 public class ControlBouton implements ActionListener, ChangeListener {
 
-    FenetreMenu fen;
+    FenetreMenu fenMenu;
     Model model;
+    FenetreSnake fenSnake;
     String backFromSkinChild;
 
 
-    public ControlBouton(FenetreMenu fen, Model model) {
-        this.fen = fen;
+    public ControlBouton(FenetreMenu fenMenu,  Model model) {
+        this.fenMenu = fenMenu;
         this.model = model;
-        this.fen.setControlButton(this);
+        this.fenMenu.setControlButton(this);
     }
 
     public void stateChanged(ChangeEvent e){
         JSlider source = (JSlider)e.getSource();
         if(!source.getValueIsAdjusting()){
-            if(e.getSource().equals(fen.getSlVolumeMusique())){
+            if(e.getSource().equals(fenMenu.getSlVolumeMusique())){
                 model.setVolumeMusique(source.getValue());
-            }else if(e.getSource().equals(fen.getSlVolumeBruits())){
+            }else if(e.getSource().equals(fenMenu.getSlVolumeBruits())){
                 model.setVolumeBruits(source.getValue());
             }
         }
@@ -34,106 +35,116 @@ public class ControlBouton implements ActionListener, ChangeListener {
     public void actionPerformed(ActionEvent a) {
 
         // Change le menu actuel vers le menu principal
-        if(a.getSource().equals(fen.getBackFromSkin()) || a.getSource().equals(fen.getbRetour()) || a.getSource().equals(fen.getBackCredits()) || a.getSource().equals(fen.getBackParam()) || a.getSource().equals(fen.getBackPlay())){
-            fen.changerMenuPrincipal();
+        if(a.getSource().equals(fenMenu.getBackFromSkin()) || a.getSource().equals(fenMenu.getbRetour()) || a.getSource().equals(fenMenu.getBackCredits()) || a.getSource().equals(fenMenu.getBackParam()) || a.getSource().equals(fenMenu.getBackPlay())){
+            fenMenu.changerMenuPrincipal();
         }
         // menu principal vers menu skin
-        if (a.getSource().equals(fen.getBoutonSkins())) {
-            fen.changerMenuSkin();
+        if (a.getSource().equals(fenMenu.getBoutonSkins())) {
+            fenMenu.changerMenuSkin();
         }
         // menu principal vers menu score
-        if (a.getSource().equals(fen.getBoutonScores())) {
-            fen.changerMenuScore();
+        if (a.getSource().equals(fenMenu.getBoutonScores())) {
+            fenMenu.changerMenuScore();
         }
         // menu principal vers menu jouer
-        if (a.getSource().equals(fen.getBoutonJouer())) {
-            fen.changerMenuJouer();
+        if (a.getSource().equals(fenMenu.getBoutonJouer())) {
+            fenMenu.changerMenuJouer();
         }
         // menu principal vers menu credits
-        if (a.getSource().equals(fen.getBoutonCredits())) {
-            fen.changerMenuCredit();
+        if (a.getSource().equals(fenMenu.getBoutonCredits())) {
+            fenMenu.changerMenuCredit();
         }
         // menu principal vers menu parametres
-        if (a.getSource().equals(fen.getBoutonParametres())) {
-            fen.changerMenuParam();
+        if (a.getSource().equals(fenMenu.getBoutonParametres())) {
+            fenMenu.changerMenuParam();
         }
         // enfant de menu skin vers menu skin
-        if(a.getSource() == fen.getBackFromSkinChild()){
+        if(a.getSource() == fenMenu.getBackFromSkinChild()){
             switch(backFromSkinChild){
                 case "skinSerpent":
-                    fen.getSkinSerpentButtonPanel().setVisible(false);
-                    fen.setBackSkin();
+                    fenMenu.getSkinSerpentButtonPanel().setVisible(false);
+                    fenMenu.setBackSkin();
                     backFromSkinChild=null;
                     break;
                 case "skinMap":
-                    fen.getSkinMapButtonPanel().setVisible(false);
-                    fen.setBackSkin();
+                    fenMenu.getSkinMapButtonPanel().setVisible(false);
+                    fenMenu.setBackSkin();
                     backFromSkinChild=null;
                     break;
                 case "skinPseudo":
-                    fen.getSkinPseudoButtonPanel().setVisible(false);
-                    fen.setBackSkin();
+                    fenMenu.getSkinPseudoButtonPanel().setVisible(false);
+                    fenMenu.setBackSkin();
                     backFromSkinChild=null;
                     break;
             }
         }
         // menu skin vers skin Serpent
-        if(a.getSource() == fen.getSkinSerpent()){
-            fen.getSkinButtonPanel().setVisible(false);
-            fen.addSkinSerpent();
+        if(a.getSource() == fenMenu.getSkinSerpent()){
+            fenMenu.getSkinButtonPanel().setVisible(false);
+            fenMenu.addSkinSerpent();
             backFromSkinChild = "skinSerpent";
         }
         // menu skin vers skin map
-        if(a.getSource() == fen.getSkinMap()){
-            fen.getSkinButtonPanel().setVisible(false);
-            fen.addSkinMap();
+        if(a.getSource() == fenMenu.getSkinMap()){
+            fenMenu.getSkinButtonPanel().setVisible(false);
+            fenMenu.addSkinMap();
             backFromSkinChild = "skinMap";
         }
         // menu skin vers skin pseudo
-        if(a.getSource() == fen.getSkinPseudo()){
-            fen.getSkinButtonPanel().setVisible(false);
-            fen.addSkinPseudo();
+        if(a.getSource() == fenMenu.getSkinPseudo()){
+            fenMenu.getSkinButtonPanel().setVisible(false);
+            fenMenu.addSkinPseudo();
             backFromSkinChild = "skinPseudo";
         }
 
-        if(a.getSource().equals(fen.getButEasy())){
-            fen.getPanPlay().setVisible(false);
-            fen.addDifficulty();
+        if(a.getSource().equals(fenMenu.getButEasy())){
+            fenMenu.getPanPlay().setVisible(false);
+            fenMenu.addDifficulty();
             model.setDifficulty("easy");
         }
-        if(a.getSource().equals(fen.getButNormal())){
-            fen.getPanPlay().setVisible(false);
-            fen.addDifficulty();
+        if(a.getSource().equals(fenMenu.getButNormal())){
+            fenMenu.getPanPlay().setVisible(false);
+            fenMenu.addDifficulty();
             model.setDifficulty("normal");
         }
-        if(a.getSource().equals(fen.getButHard())){
-            fen.getPanPlay().setVisible(false);
-            fen.addDifficulty();
+        if(a.getSource().equals(fenMenu.getButHard())){
+            fenMenu.getPanPlay().setVisible(false);
+            fenMenu.addDifficulty();
             model.setDifficulty("hard");
         }
-        if(a.getSource().equals(fen.getButTrad())){
-            // lance le leu traditionelle
+        if(a.getSource().equals(fenMenu.getButTrad())){
+            // lance le jeu traditionelle
             model.setMode("Traditionnel");
-            fen.dispose();
-            FenetreSnake fenetreSnake = new FenetreSnake(model);
+            fenMenu.setVisible(false);
+            fenSnake  = new FenetreSnake(fenMenu);
+            ControlSnake controlSnake = new ControlSnake(fenSnake,model);
+
         }
-        if(a.getSource().equals(fen.getButLaby())){
+        if(a.getSource().equals(fenMenu.getButLaby())){
             // lance le jeu labyrinthe
             model.setMode("Labyrinthe");
         }
-        if(a.getSource().equals(fen.getButChrono())){
+        if(a.getSource().equals(fenMenu.getButChrono())){
             // lance le jeu labyrinthe
             model.setMode("Chrono");
         }
-        if(a.getSource().equals(fen.getButDuo())){
+        if(a.getSource().equals(fenMenu.getButDuo())){
             // lance le jeu labyrinthe
             model.setMode("Duo");
         }
-        if(a.getSource().equals(fen.getBackDifficulty())){
+        if(a.getSource().equals(fenMenu.getBackDifficulty())){
             System.out.println("test");
-            fen.getPanDifficulty().setVisible(false);
-            fen.setBackPlay();
+            fenMenu.getPanDifficulty().setVisible(false);
+            fenMenu.setBackPlay();
         }
 
+    }
+
+    public FenetreSnake getFenSnake() {
+        return fenSnake;
+    }
+
+    public void setFenSnake(FenetreSnake fenSnake) {
+        this.fenSnake = fenSnake;
     }
 }
